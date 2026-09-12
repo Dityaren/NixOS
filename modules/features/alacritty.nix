b881fd8d5ee8ef0caded185426cda66afa54d0ed
ein <ein@ein.dev>
2026-09-12 14:45:00 +0700

    fix: homemanager doesnt replace old file fix

--- modules/features/alacritty.nix
+++ modules/features/alacritty.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
-{
 
+{
   flake.nixosModules.alacritty =
     {
       vars,
@@ -11,18 +11,20 @@
       home-manager.users.${vars.username} = {
         programs.alacritty = {
           enable = true;
+
           settings = {
             window = {
               opacity = 0.8;
               blur = true;
-
             };
+
             font = {
-              size = 16;
+              size = 18;
             };
           };
-
         };
+
+        xdg.configFile."alacritty/alacritty.toml".force = true;
       };
     };
 }
