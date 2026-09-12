dfbf3ec8c3f21d51556fe31ac4347794b10f5f53
ein <ein@ein.dev>
2026-09-12 17:00:03 +0700

    chore: minor redundant config

--- modules/hosts/snow/vars.nix
+++ modules/hosts/snow/vars.nix
@@ -5,11 +5,5 @@
     hostname = "snow";
     stateVersion = "26.05";
     flakeRoot = "/home/lake/dotfiles/nixos/";
-
-    paths = {
-      noctalia = {
-        config = "/home/lake/dotfiles/nixos/modules/features/noctalia/config.toml";
-      };
-    };
   };
 }
