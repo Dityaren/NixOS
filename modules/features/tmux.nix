d0469d10975372f46a7e7a94920712211ca796d1
ein <ein@ein.dev>
2026-09-14 20:36:28 +0700

    style: change tmux style

--- modules/features/tmux.nix
+++ modules/features/tmux.nix
@@ -50,17 +50,14 @@
 
             set -ag terminal-overrides ",xterm-256color:RGB"
             set -ag terminal-overrides ",xterm*:Tc"
-            set -g @kanagawa_bg "#16161D"
-            set -g @kanagawa_bg_alt "#1F1F28"
-            set -g @kanagawa_border "#2A2A37"
+            set -g @black "#000000"
+            set -g @dark "#0A0A0A"
+            set -g @gray "#1A1A1A"
+            set -g @gray-light "#2A2A2A"
 
-            set -g @kanagawa_fg "#DCD7BA"
-            set -g @kanagawa_muted "#727169"
-
-            set -g @kanagawa_blue "#7E9CD8"
-            set -g @kanagawa_cyan "#7FB4CA"
-            set -g @kanagawa_yellow "#DCA561"
-            set -g @kanagawa_green "#6A9589"
+            set -g @muted "#666666"
+            set -g @fg "#B3B3B3"
+            set -g @white "#D4D4D4"
 
             set -g base-index 1
             setw -g pane-base-index 1
@@ -101,50 +98,50 @@
             set -g status-position top
             set -g status-interval 5
             set -g status-justify left
-            set -g status-left-length 40
-            set -g status-right-length 100
+            set -g status-left-length 30
+            set -g status-right-length 80
 
             set -g status-style \
-              "bg=#{@kanagawa_bg},fg=#{@kanagawa_fg}"
+              "bg=#{@black},fg=#{@fg}"
 
             set -g status-left-style \
-              "bg=#{@kanagawa_bg},fg=#{@kanagawa_fg}"
+              "bg=#{@black},fg=#{@fg}"
 
             set -g status-right-style \
-              "bg=#{@kanagawa_bg},fg=#{@kanagawa_fg}"
+              "bg=#{@black},fg=#{@fg}"
 
             set -g status-left \
-              "#[fg=#{@kanagawa_blue},bold] 󱄅 #[fg=#{@kanagawa_border}]│ "
+              "#[fg=#{@white},bold] tmux #[fg=#{@muted}]"
 
             setw -g window-status-format \
-              "#[fg=#{@kanagawa_muted}]#I:#W "
+              "#[fg=#{@muted}] #I:#W "
 
             setw -g window-status-current-format \
-              "#[fg=#{@kanagawa_blue},bold]#I:#W #[nobold]"
+              "#[fg=#{@white},bold] #I:#W "
 
             setw -g window-status-separator ""
 
             set -g status-right \
-              "#[fg=#{@kanagawa_muted}]#{b:pane_current_path} #[fg=#{@kanagawa_border}]│ #[fg=#{@kanagawa_yellow}]%H:%M "
+              "#[fg=#{@muted}]#{b:pane_current_path}  #[fg=#{@gray-light}]│  #[fg=#{@fg}]%H:%M "
 
             set -g pane-border-lines single
 
             set -g pane-border-style \
-              "fg=#{@kanagawa_border}"
+              "fg=#{@gray-light}"
 
             set -g pane-active-border-style \
-              "fg=#{@kanagawa_blue}"
+              "fg=#{@muted}"
 
             setw -g automatic-rename on
 
             setw -g automatic-rename-format \
-              '
+              '#W'
 
             set -g message-style \
-              "bg=#{@kanagawa_bg_alt},fg=#{@kanagawa_fg}"
+              "bg=#{@gray},fg=#{@white}"
 
             set -g message-command-style \
-              "bg=#{@kanagawa_bg_alt},fg=#{@kanagawa_fg}"
+              "bg=#{@gray},fg=#{@white}"
 
             set -g @resurrect-capture-pane-contents 'on'
 
