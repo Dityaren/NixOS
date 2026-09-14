72784e06c3afac42dd45d59d0d97510725c755b3
ein <ein@ein.dev>
2026-09-14 13:18:43 +0700

    fix: toggleable mini.files, swap explorers bind

--- modules/features/nixvim.nix
+++ modules/features/nixvim.nix
@@ -1106,17 +1106,30 @@
           }
 
           {
-            key = "<leader>e";
+            key = "<leader>o";
             mode = "n";
-            action = "<cmd>lua MiniFiles.open()<CR>";
-            options.desc = "Open file explorer";
+            action.__raw = ''
+              function()
+                if not MiniFiles.close() then
+                  MiniFiles.open()
+                end
+              end
+            '';
+            options.desc = "Toggle mini.files";
           }
 
           {
-            key = "<leader>o";
+            key = "<leader>e";
             mode = "n";
-            action = "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0)); MiniFiles.reveal_cwd()<CR>";
-            options.desc = "Reveal current file";
+            action.__raw = ''
+              function()
+                if not MiniFiles.close() then
+                  MiniFiles.open(vim.api.nvim_buf_get_name(0))
+                  MiniFiles.reveal_cwd()
+                end
+              end
+            '';
+            options.desc = "Toggle reveal current file";
           }
 
           {
