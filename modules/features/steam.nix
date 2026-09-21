32bd54bb43eabcf36aeb770b17c390f41e9d81af
Dityaren <DitoAdityaSembiringKembaren1@gmail.com>
2026-09-21 22:52:28 +0700

    fix: adding wineWOW64packages into steam modules

--- modules/features/steam.nix
+++ modules/features/steam.nix
@@ -1,24 +1,33 @@
 { self, inputs, ... }: {
-  flake.nixosModules.steam = { pkgs, ... }: {
-    programs = {
-      steam = {
-        enable = true;
-        extraCompatPackages = [
-          pkgs.dwproton-bin
-        ];
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
+  flake.nixosModules.steam =
+    { pkgs, ... }:
+    {
+      programs = {
+        steam = {
+          enable = true;
+          extraCompatPackages = [
+            pkgs.dwproton-bin
+          ];
+          remotePlay.openFirewall = true;
+          dedicatedServer.openFirewall = true;
+        };
+
+        gamemode.enable = true;
       };
 
-      gamemode.enable = true;
+      environment.systemPackages = with pkgs; [
+        lutris
+        steam-run
+        protonup-qt
+        gamescope
+        mangohud
+        goverlay
+        vulkan-tools
+        vulkan-loader
+        vulkan-validation-layers
+        mesa
+        wineWow64Packages.stable
+        winetricks
+      ];
     };
-
-    environment.systemPackages = with pkgs; [
-      steam-run
-      protonup-qt
-      gamescope
-      mangohud
-      goverlay
-    ];
-  };
 }
